4959ce4b38d3fe69a5d4ae4b6cceba05ac69e76c
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-02-24 15:27:29 -0800

    Modernise project names

--- CaseConversion/Properties/AssemblyInfo.cs
+++ CaseConversion/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CaseConversion")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Andeart.CaseConversions")]
+[assembly: AssemblyDescription("Easily convert strings into various casing styles: camelCase, PascalCase, lower_snake_case, _underscoreCamelCase.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CaseConversion")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Andeart")]
+[assembly: AssemblyProduct("Andeart.CaseConversions")]
+[assembly: AssemblyCopyright("Copyright © ANURAG DEVANAPALLY 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
